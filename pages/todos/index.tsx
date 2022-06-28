3b87b4a67f08cd970f6781186f999420847cc89b
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-06-29 08:58:07 +0900

    FirebaseのデータをNextでレンダリング

--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,15 +1,34 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+
+import { db } from "../../lib/firebase";
 
 const Home: NextPage = () => {
+  const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+
+  useEffect(() => {
+    const unSub = db.collection("tasks").onSnapshot((snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
+      );
+    });
+    return () => unSub(); /* アンマウントしたら、firebaseの監視を停止 */
+  }, []);
+
   return (
-    <Box>
+    <>
       <Head>
         <title>TODOリスト</title>
       </Head>
       <Heading>TODO一覧</Heading>
-    </Box>
+      <Box>
+        {tasks.map((task, index) => (
+          <Text key={index}>{task.title}</Text>
+        ))}
+      </Box>
+    </>
   );
 };
 
